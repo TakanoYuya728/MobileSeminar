30ec0ef485e7aef5ce770d19f4cbe88d6e4f708e
takano <takano>
2015-06-03 05:01:50 +0900

    js組み合わせ

---
+++ jsとの組み合わせ/index.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>sss</title>
+        <script type="text/javascript" src="js/newjavascript.js" ></script>
+    </head>
+    <body>
+        <script>  
+        ko();
+        </script>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
